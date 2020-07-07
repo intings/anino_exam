33ea930c41f06c93a0222f517671f0f17221344f
Philip Inting <philip.inting@nuworks.ph>
2020-07-08 00:41:42 +0800

    First build with quick UI

--- Assets/Scripts/Reel.cs
+++ Assets/Scripts/Reel.cs
@@ -9,11 +9,13 @@ public class Reel : MonoBehaviour
     [SerializeField] private int reelNumber;
     [SerializeField] private Symbol[] symbols;
     private int _spinDuration;
+    private int _rinseRepeat;
     public Action<int, int[]> stopReel;
     private const int DUPLICATE_COUNT = 5;
     private const float GRID_SIZE = 1.5f;
     private const float TIME_INTERVAL = 0.005f; 
-    private const int TICKS_TO_FULL_SPIN = 6;
+    private const int TICKS_TO_MOVE_ONE_SYMBOL = 6;
+    
     
     private void Awake()
     {
@@ -31,52 +33,88 @@ public class Reel : MonoBehaviour
         }
     }
 
-    public void StartSpin(int remainingLoops)
+    public void StopSpin()
     {
-        StartCoroutine(Spin(remainingLoops));
+        _rinseRepeat = 3;
     }
 
-    private IEnumerator Spin(int remainingLoops)
+    /// <summary>
+    /// Will Spin and Stop accordingly to achieve target result
+    /// </summary>
+    /// <param name="targetResult">Positon of the symbol on the reel</param>
+    /// <param name="rinseRepeat">How many times reel will do full rotation</param>
+    public void StartSpin(int targetResult, int rinseRepeat)
     {
-        const float tick = GRID_SIZE / TICKS_TO_FULL_SPIN;
+        _rinseRepeat = rinseRepeat;
+        StartCoroutine(Spin(targetResult));
+    }
+    
+    private IEnumerator Spin(int targetResult)
+    {
+        //Debug.Log(symbols[targetResult].name);
+        for (var i = 0; i < _rinseRepeat; i++)
+        {
+            var targetDistance = targetResult - (((int) (transform.position.y / GRID_SIZE)) + 1);
+            if (targetDistance <= 0)
+                targetDistance += (symbols.Length - DUPLICATE_COUNT);
+            var remainingLoops = targetDistance * TICKS_TO_MOVE_ONE_SYMBOL;
+            const float tickDistance = GRID_SIZE / TICKS_TO_MOVE_ONE_SYMBOL;
+            for (var j = 0; j < remainingLoops; j++)
+            {
+                Tick(tickDistance);
+                yield return new WaitForSeconds(TIME_INTERVAL);
+            }
+        }
+        GetReelResult();
+    }
+    
+    private IEnumerator Spin2(int targetResult)
+    {
+        //Debug.Log(targetResult);
+        Debug.Log(symbols[targetResult].name);
+        Debug.Log(transform.position.y);
+        var targetDistance = targetResult - ((int) (transform.position.y / GRID_SIZE)) - 1;
+        if (targetDistance <= 0)
+            targetDistance += symbols.Length - DUPLICATE_COUNT;
+        var remainingLoops = targetDistance * TICKS_TO_MOVE_ONE_SYMBOL;
+        const float tickDistance = GRID_SIZE / TICKS_TO_MOVE_ONE_SYMBOL;
         for (var i = 0; i < remainingLoops; i++)
         {
-            Tick(tick);
+            Tick(tickDistance);
             yield return new WaitForSeconds(TIME_INTERVAL);
         }
 
         
-        _spinDuration = Random.Range(30, 50);
-        var addSpinTime = _spinDuration % TICKS_TO_FULL_SPIN;
-        _spinDuration += (TICKS_TO_FULL_SPIN - addSpinTime);
-
-        for (var i = 0; i < _spinDuration; i++)
-        {
-            float timeIntervalToSLowSpin = 0;
-            var j = i;
-            Tick(tick, () =>
-            {
-                if (j > Mathf.RoundToInt(_spinDuration * 0.25f))
-                    timeIntervalToSLowSpin = 0.05f;
-                if (j > Mathf.RoundToInt(_spinDuration * 0.5f))
-                    timeIntervalToSLowSpin = 0.1f;
-                if (j > Mathf.RoundToInt(_spinDuration * 0.75f))
-                    timeIntervalToSLowSpin = 0.15f;
-                if (j > Mathf.RoundToInt(_spinDuration * 0.95f))
-                    timeIntervalToSLowSpin = 0.2f;
-            });
-            yield return new WaitForSeconds(timeIntervalToSLowSpin);
-        }
+        // _spinDuration = Random.Range(30, 50);
+        // var addSpinTime = _spinDuration % TICKS_TO_MOVE_ONE_SYMBOL;
+        // _spinDuration += (TICKS_TO_MOVE_ONE_SYMBOL - addSpinTime);
+        //
+        // for (var i = 0; i < _spinDuration; i++)
+        // {
+        //     float timeIntervalToSLowSpin = 0;
+        //     var j = i;
+        //     Tick(tick, () =>
+        //     {
+        //         if (j > Mathf.RoundToInt(_spinDuration * 0.25f))
+        //             timeIntervalToSLowSpin = 0.05f;
+        //         if (j > Mathf.RoundToInt(_spinDuration * 0.5f))
+        //             timeIntervalToSLowSpin = 0.1f;
+        //         if (j > Mathf.RoundToInt(_spinDuration * 0.75f))
+        //             timeIntervalToSLowSpin = 0.15f;
+        //         if (j > Mathf.RoundToInt(_spinDuration * 0.95f))
+        //             timeIntervalToSLowSpin = 0.2f;
+        //     });
+        //     yield return new WaitForSeconds(timeIntervalToSLowSpin);
+        // }
         GetReelResult();
     }
 
     private void GetReelResult()
     {
-        var reelPosition = (int) (transform.position.y / GRID_SIZE);
         var reelResults = new int[3];
         for (var i = 0; i < 3; i++)
         {
-            reelResults[i] = symbols[reelPosition + i + 1].Value;
+            reelResults[i] = symbols[(int) (transform.position.y / GRID_SIZE) + i + 1].Value;
         }
         stopReel?.Invoke(
             reelNumber-1,
@@ -84,15 +122,20 @@ public class Reel : MonoBehaviour
         );
     }
 
-    private void Tick(float tick, Action reduceSpeed = null)
+    private void Tick(float tickDistance, Action reduceSpeed = null)
     {
         var position = transform.position;
         if (position.y < 0)
         {
-            var yOffset = ((symbols.Length-DUPLICATE_COUNT) * GRID_SIZE) - tick;
+            var yOffset = ((symbols.Length-DUPLICATE_COUNT) * GRID_SIZE) - tickDistance;
             position = transform.position = new Vector3(position.x, yOffset);
         }
         reduceSpeed?.Invoke();
-        transform.position = new Vector3(position.x, position.y - tick);
+        transform.position = new Vector3(position.x, position.y - tickDistance);
+    }
+
+    private void OnDestroy()
+    {
+        stopReel = null;
     }
 }
--- Assets/Scripts/ReelController.cs
+++ Assets/Scripts/ReelController.cs
@@ -1,37 +1,73 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using ScriptableObjects;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReelController : MonoBehaviour
 {
     [SerializeField] private Reel[] reels;
     [SerializeField] private PayLine[] payLines;
+    [SerializeField] private Button spinButton;
+    [SerializeField] private TextMeshProUGUI spinButtonText;
+    [SerializeField] private TextMeshProUGUI coinsText;
+    [SerializeField] private TextMeshProUGUI winningsText;
+    [SerializeField] private TextMeshProUGUI betText;
+    private int _betAmount = 10;
+    private int _coins = 100;
     private int _reelsStopped = 0;
     private bool _canSpin = true; //enable again when done displaying wins
+    private bool _canStop = false;
     private int[,] _results;
     private List<Win> _wins;
 
-    // private void Awake()
-    // {
-    //     PayLinesData.    .payLines = payLines;
-    // }
-
-    public void Spin()
+    private void Awake()
     {
-        if (!_canSpin) return;
+        PayLinesData.Instance.payLines = payLines;
+    }
+
+    private void Spin()
+    {
+        if (!_canSpin || _betAmount == 0) return;
         _canSpin = false;
+        _coins -= _betAmount;
+        coinsText.text = "COINS : " + _coins;
         _results = new int[3, 5];
+        spinButton.onClick.RemoveListener(Spin);
+        spinButton.onClick.AddListener(Stop);
+        spinButtonText.text = "STOP";
         _wins = new List<Win>();
         var i = 0;
+        var targetResults = new List<int>();
+        var reelLength = SymbolsDataHolder.Instance.symbolsData.Length;
         foreach (var reel in reels)
         {
+            var targetResult = SetTargetBeforeSpinning(reelLength);
+            targetResults.Add(targetResult);
             reel.stopReel = StopReel;
-            reel.StartSpin(30 + (i * 30));
+            reel.StartSpin(targetResult, i + 3);
+            //reel.StartSpin(30 + (i * 30));
             i++;
+        }
+        _canStop = true;
+    }
+
+    private void Stop()
+    {
+        if (!_canStop) return;
+        _canStop = false;
+        spinButton.enabled = false;
+        foreach (var reel in reels)
+        {
+            reel.StopSpin();
         }
     }
 
+    private static int SetTargetBeforeSpinning(int reelLength)
+    {
+        return Random.Range(0, reelLength - 1);
+    }
     private void StopReel(int reelNumber, int[] rows)
     {
         _reelsStopped++;
@@ -64,22 +100,50 @@ public class ReelController : MonoBehaviour
                 select new {Value = g.Key, Count = count};
             foreach (var item in query)
             {
-                Debug.Log("Value: " + item.Value + " Count: " + item.Count);
-                
                 _wins.Add(new Win(payLine, item.Value, item.Count));
             }
         }
-        HighLightSymbols();
+        DisplayWinnings();
+        CheckIfBetAllowed();
         _canSpin = true;
+        spinButton.enabled = true;
+        spinButton.onClick.RemoveListener(Stop);
+        spinButton.onClick.AddListener(Spin);
+        spinButtonText.text = "SPIN";
     }
 
-    private void HighLightSymbols()
+    private void DisplayWinnings()
     {
         var symbolsData = SymbolsDataHolder.Instance.symbolsData;
+        var winAmount = 0;
         foreach (var win in _wins)
         {
-            var a = symbolsData[win.Value -1].PayOut[win.Count -1];
-            Debug.Log(a);
+            var linePayout = symbolsData[win.Value - 1].PayOut[win.Count - 1];
+            linePayout *= (_betAmount / 10);
+            winAmount += linePayout;
         }
+
+        _coins += winAmount;
+        winningsText.text = "WINNINGS : " + winAmount;
+        coinsText.text = "COINS : " + _coins;
+    }
+
+    public void AddBet(int amount)
+    {
+        _betAmount += amount;
+        CheckIfBetAllowed();
+    }
+
+    private bool CheckIfBetAllowed()
+    {
+        var allow = true;
+        if (_betAmount < 0) _betAmount = 0;
+        while (_coins < _betAmount)
+        {
+            allow = false;
+            _betAmount -= 10;
+        }
+        betText.text = "BET : " + _betAmount;
+        return allow;
     }
 }
